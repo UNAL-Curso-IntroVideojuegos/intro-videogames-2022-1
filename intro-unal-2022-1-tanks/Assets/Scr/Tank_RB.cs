ef13f4805db4fba319eca92c68411c107adb4954
Alejandro Alvarez <juaalvarezme@gmail.com>
2022-03-28 17:02:13 -0500

    Session #3: Transform and some Physics

---
+++ intro-unal-2022-1-tanks/Assets/Scr/Tank_RB.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank_RB : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb.velocity = new Vector2(0, 1);
+    }
+
+    private void Update()
+    {
+        //transform.position = Vector3.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        //_rb.velocity = Vector3.zero;
+    }
+}
