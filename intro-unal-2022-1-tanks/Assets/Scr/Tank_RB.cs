2ade80876cb1d0ae4cad43d3d29b8cb69c75bed5
Alejandro Alvarez <juaalvarezme@gmail.com>
2022-04-02 09:16:08 -0500

    Added Tank with rigidbody

--- intro-unal-2022-1-tanks/Assets/Scr/Tank_RB.cs
+++ intro-unal-2022-1-tanks/Assets/Scr/Tank_RB.cs
@@ -6,20 +6,50 @@ using UnityEngine;
 public class Tank_RB : MonoBehaviour
 {
     [SerializeField]
+    private float speed = 2;
+    [SerializeField]
     private Rigidbody2D _rb;
+    [SerializeField]
+    private Camera _cam;
+    [SerializeField]
+    private Transform _turret;
 
+    private Vector2 _velocity;
+    
     private void Start()
     {
-        _rb.velocity = new Vector2(0, 1);
     }
 
     private void Update()
     {
-        //transform.position = Vector3.zero;
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 mousePos = Input.mousePosition;
+        Vector3 mouseWorldPos = _cam.ScreenToWorldPoint(mousePos);
+        mouseWorldPos.z = 0;
+        
+        Vector2 _dir  = new Vector2(horizontal, vertical);
+        _velocity.Normalize();
+        _velocity = speed * _dir; // _velocity * speed;
+        
+        //Rotation:
+        Vector3 aimVector = mouseWorldPos - transform.position;
+        float angle = Mathf.Atan2(aimVector.y, aimVector.x) * Mathf.Rad2Deg - 90;
+        
+        Vector3 rot = _turret.eulerAngles;
+        rot.z = angle;
+        _turret.eulerAngles = rot;
     }
 
     private void FixedUpdate()
     {
-        //_rb.velocity = Vector3.zero;
+        //Dynimac
+        _rb.velocity = _velocity;
+        //_rb.AddForce();
+        
+        //Kinematic
+        //Vector2 pos = _rb.position + _velocity * Time.fixedDeltaTime;
+        //_rb.MovePosition(pos);
     }
 }
