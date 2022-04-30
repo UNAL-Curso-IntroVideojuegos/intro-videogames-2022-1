455785f1065774dceac7cd0c40b9be648f3ce484
Alejandro Alvarez <juaalvarezme@gmail.com>
2022-04-30 09:50:21 -0500

    Added NavMesh and PathFindingController

--- intro-unal-2022-1-shooter/Assets/Scr/Enemy/PathFindingController.cs
+++ intro-unal-2022-1-shooter/Assets/Scr/Enemy/PathFindingController.cs
@@ -1,39 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class ClickToMove : MonoBehaviour
+public class PathFindingController : MonoBehaviour
 {
     private NavMeshAgent _agent;
-    private Camera _cam;
-
+    private Action _onArrive;
+    
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
-        _cam = Camera.main;
     }
-
+    
     void Update()
     {
         bool isStopped = _agent.isStopped || 
                          (_agent.remainingDistance <= _agent.stoppingDistance && _agent.velocity.sqrMagnitude == 0);
-
-        //if (isStopped)
-        if(Input.GetMouseButtonDown(0)) //Si oprimi click izq
-        {
-            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitInfo;
-            if (Physics.Raycast(ray, out hitInfo))
-            {
-                _agent.SetDestination(hitInfo.point);
-            }
-        }
         
         //Si efectivamente complete el recorrido
         if (!_agent.pathPending && isStopped)
         {
-            Debug.LogError("Llegue!");
+            if (_onArrive != null)
+            {
+                _onArrive();
+                _onArrive = null;
+            }
         }
+    }
+
+    public void GoTo(Vector3 position, Action callback)
+    {
+        _agent.isStopped = false;
+        _agent.SetDestination(position);
+        _onArrive = callback;
+    }
+
+    public void Stop()
+    {
+        _agent.isStopped = true;
     }
 }
