93d9efd009adb4edf397428c6aefa14ccbff6e47
Daniel Estrada <daestradam@unal.edu.co>
2022-05-14 22:32:20 -0500

    Punto 4

--- intro-unal-2022-1-shooter/Assets/Scr/Enemy/StateMachineController.cs
+++ intro-unal-2022-1-shooter/Assets/Scr/Enemy/StateMachineController.cs
@@ -18,7 +18,7 @@ public class StateMachineController
         _states.Add(EnemyStateType.Idle, new EnemyIdleState());
         _states.Add(EnemyStateType.Chase, new EnemyChaseState());
         _states.Add(EnemyStateType.Attack, new EnemyAttackState());
-        //_states.Add(EnemyStateType.Patrol, );
+        _states.Add(EnemyStateType.Patrol, new EnemyPatrolState());
 
 
         ChangeToState(_agent.AgentConfig.initialState);
---
+++ intro-unal-2022-1-shooter/Assets/Scr/Enemy/States/EnemyPatrolState.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrolState : IEnemyState
+{
+    private float _navMeshRefreshTimer = 0;
+
+    private int _currentPointIndex = 0;
+
+    private Vector3 GetAgentNextPoint(EnemyAgent agent)
+    {
+        //If we don't have a Path return (0, 0, 0)
+        if (agent.AgentConfig.PathPoints == null || agent.AgentConfig.PathPoints.Count == 0)
+        {
+            return Vector3.zero;
+        }
+
+        //Move through the path. When reach the end -> start from 0 again
+        _currentPointIndex = (_currentPointIndex + 1) % agent.AgentConfig.PathPoints.Count;
+        return agent.AgentConfig.PathPoints[_currentPointIndex].position;
+    }
+
+    public void OnEnter(EnemyAgent agent)
+    {
+        Debug.Log("Patrol: OnEnter");
+        _navMeshRefreshTimer = 0;
+    }
+
+    public void OnUpdate(EnemyAgent agent)
+    {
+        Debug.Log("Patrol: OnUpdate");
+        _navMeshRefreshTimer -= Time.deltaTime;
+
+        //Pasaron X segundos, volver a Patrol
+        if (_navMeshRefreshTimer <= 0)
+        {
+            _navMeshRefreshTimer = 5;
+            agent.PathFindingController.GoTo(GetAgentNextPoint(agent), null);
+            
+        }
+
+    }
+
+    public void OnExit(EnemyAgent agent)
+    {
+        Debug.Log("Patrol: OnExit");
+        agent.PathFindingController.Stop();
+    }
+}
