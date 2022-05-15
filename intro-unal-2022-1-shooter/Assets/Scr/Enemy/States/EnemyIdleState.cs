f71614b8b136ccc1a277e94acbcca4f1b240ebc1
Daniel Estrada <daestradam@unal.edu.co>
2022-05-14 22:00:38 -0500

    Punto 3

--- intro-unal-2022-1-shooter/Assets/Scr/Enemy/States/EnemyIdleState.cs
+++ intro-unal-2022-1-shooter/Assets/Scr/Enemy/States/EnemyIdleState.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class EnemyIdleState : IEnemyState
 {
+    private float _navMeshRefreshTimer = 0;
+
     public void OnEnter(EnemyAgent agent)
     {
         Debug.Log("Idle: OnEnter");
+        _navMeshRefreshTimer = 0;
     }
 
     public void OnUpdate(EnemyAgent agent)
     {
         Debug.Log("Idle: OnUpdate");
-        
+        _navMeshRefreshTimer -= Time.deltaTime;
+
+        //Pasaron X segundos, volver a Patrol
+        if (_navMeshRefreshTimer <= 0)
+        {
+            _navMeshRefreshTimer = agent.AgentConfig.IdleTime;
+            agent.StateMachineController.ChangeToState(EnemyStateType.Patrol);
+        }
+
         //Vi al Player -> Pasar al estado Chase! 
         if (agent.IsLookingTarget())
         {
